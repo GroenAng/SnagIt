089b42d647be9358e73fefbc7af70dd3251e84bb
GroenAng <groen.angelique@gmail.com>
2025-04-29 18:36:26 +0200

    Successful production build for SnagIt

--- app/dashboard/favorites/page.tsx
+++ app/dashboard/favorites/page.tsx
@@ -1,13 +1,9 @@
-import dynamic from "next/dynamic";
-
-const Motion = dynamic(() => import("framer-motion").then((mod) => mod.motion), { ssr: false });
-
 export default function DashboardPage() {
     return (
         <div>
-            <Motion.div>
+            <div>
                 <h1>Dashboard</h1>
-            </Motion.div>
+            </div>
         </div>
     );
 }
