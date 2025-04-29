d45dc5ff661111728812b5c2f4d23e6578383442
GroenAng <groen.angelique@gmail.com>
2025-04-29 18:21:03 +0200

    Fix build error by removing framer-motion and correcting imports

---
+++ components/recent-deals.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecentDeals = () => {
+    return (
+        <div>
+            <h2>Recent Deals</h2>
+            <p>Here are some recent deals for you!</p>
+        </div>
+    );
+};
+
+export default RecentDeals;
